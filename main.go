29534961b33b7a1a7d3ee7e3998af8753dffb586
Ryan Travitz <rtravitz@gmail.com>
2016-12-31 19:21:06 -0700

    Make db initialization use environment variables

--- main.go
+++ main.go
@@ -3,36 +3,45 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
+	"strconv"
 
 	_ "github.com/lib/pq"
 	"github.com/rtravitz/coparkfinder/db"
 	"github.com/rtravitz/coparkfinder/seed"
 )
 
-const (
-	host   = "localhost"
-	port   = 5432
-	user   = "rtravitz"
-	dbname = "parkfinderdev"
-)
-
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"dbname=%s sslmode=disable",
-		host, port, user, dbname)
-
-	var err error
-	db.DBCon, err = sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
+	port, err := strconv.Atoi(os.Getenv("PARKFINDER_PORT"))
+	checkErr(err)
+	psql := psqlInfo{host: os.Getenv("PARKFINDER_HOST"), port: port,
+		user: os.Getenv("PARKFINDER_USER"), dbname: os.Getenv("PARKFINDER_DBNAME")}
+	db.DBCon, err = sql.Open("postgres", psql.info())
+	checkErr(err)
 	defer db.DBCon.Close()
 	err = db.DBCon.Ping()
-	if err != nil {
-		panic(err)
-	}
+	checkErr(err)
 
 	fmt.Println("Successfully connected!")
 
 	seed.Seed()
+}
+
+type psqlInfo struct {
+	host   string
+	port   int
+	user   string
+	dbname string
+}
+
+func (p psqlInfo) info() string {
+	return fmt.Sprintf("host=%s port=%d user=%s "+
+		"dbname=%s sslmode=disable",
+		p.host, p.port, p.user, p.dbname)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
