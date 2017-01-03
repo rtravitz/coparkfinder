90a43a2484fa66f1f833934ae5c87137b1bdc3e7
Ryan Travitz <rtravitz@gmail.com>
2017-01-03 15:09:08 -0700

    Add parks_activities migration

---
+++ db/migrations/20170103150734_CreateParksActivitiesTable.sql
@@ -0,0 +1,15 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE Table parks_activities (
+  id serial,
+  park_id integer,
+  activity_id integer,
+  PRIMARY KEY(id)
+);
+
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE activities_facilities;
+
