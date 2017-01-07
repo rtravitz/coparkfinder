1b489e4f8027a2d58f5752ece1f8967a2cd2bdc5
Ryan Travitz <rtravitz@gmail.com>
2017-01-07 14:59:14 -0700

    Move router to an individual file

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,36 +2,9 @@ package handler
 
 import (
 	"encoding/json"
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
 	"github.com/rtravitz/coparkfinder/models"
 	"net/http"
 )
-
-type Handler struct {
-	DB *models.DB
-}
-
-func NewHandler(db *models.DB) *Handler {
-	return &Handler{
-		DB: db,
-	}
-}
-
-func (h *Handler) NewRouter() http.Handler {
-	r := mux.NewRouter()
-	r.HandleFunc("/api/v1/parks", h.ParksIndex).
-		Methods("GET")
-	r.HandleFunc("/api/v1/activities", h.ActivitiesIndex).
-		Methods("GET")
-	r.HandleFunc("/api/v1/facilities", h.FacilitiesIndex).
-		Methods("GET")
-
-	handler := handlers.HTTPMethodOverrideHandler(r)
-	o := handlers.AllowedOrigins([]string{"*"})
-	handler = handlers.CORS(o)(r)
-	return handler
-}
 
 func (h *Handler) ActivitiesIndex(w http.ResponseWriter, r *http.Request) {
 	var activities []*models.Activity
---
+++ handlers/router.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/rtravitz/coparkfinder/models"
+	"net/http"
+)
+
+type Handler struct {
+	DB *models.DB
+}
+
+func NewHandler(db *models.DB) *Handler {
+	return &Handler{
+		DB: db,
+	}
+}
+
+func (h *Handler) NewRouter() http.Handler {
+	r := mux.NewRouter()
+	r.HandleFunc("/api/v1/parks", h.ParksIndex).
+		Methods("GET")
+	r.HandleFunc("/api/v1/activities", h.ActivitiesIndex).
+		Methods("GET")
+	r.HandleFunc("/api/v1/facilities", h.FacilitiesIndex).
+		Methods("GET")
+
+	handler := handlers.HTTPMethodOverrideHandler(r)
+	o := handlers.AllowedOrigins([]string{"*"})
+	handler = handlers.CORS(o)(r)
+	return handler
+}
