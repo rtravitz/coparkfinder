d9feb73194d006126b4c0808b1ba37e90a45ecfd
Ryan Travitz <rtravitz@gmail.com>
2017-01-11 22:16:02 -0700

    Set test format

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -1,25 +1,50 @@
 package handler
 
 import (
+	"github.com/rtravitz/coparkfinder/models"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestActivitiesIndexHandler(t *testing.T) {
-	req, err := http.NewRequest("GET", "/api/v1/activities", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/api/v1/activities", nil)
+	env := Env{DB: &mockDB{}}
+	router := env.NewRouter()
+	router.ServeHTTP(w, r)
 
-	rr := httptest.NewRecorder()
-	h := NewHandler(tdb)
-	handler := http.HandlerFunc(h.ActivitiesIndex)
-
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
+	if status := w.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
+}
 
+type mockDB struct{}
+
+func (mdb *mockDB) AllParks() ([]*models.Park, error) {
+	return nil, nil
+}
+
+func (mdb *mockDB) FindPark(where string, params ...interface{}) (*models.Park, error) {
+	return nil, nil
+}
+
+func (mdb *mockDB) FindParks(params map[string][]string) ([]*models.Park, error) {
+	return nil, nil
+}
+
+func (mdb *mockDB) FindParkActivities(parkID int) ([]*models.Activity, error) {
+	return nil, nil
+}
+
+func (mdb *mockDB) FindParkFacilities(parkID int) ([]*models.Facility, error) {
+	return nil, nil
+}
+
+func (mdb *mockDB) AllFacilities() ([]*models.Facility, error) {
+	return nil, nil
+}
+
+func (mdb *mockDB) AllActivities() ([]*models.Activity, error) {
+	return nil, nil
 }
