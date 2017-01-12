663222f92976458a77d931979c20c05b6f016bd0
Ryan Travitz <rtravitz@gmail.com>
2017-01-11 23:41:03 -0700

    Pass first handler test

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -1,9 +1,15 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/rtravitz/coparkfinder/models"
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"path/filepath"
+	"reflect"
+	"runtime"
 	"testing"
 )
 
@@ -13,10 +19,20 @@ func TestActivitiesIndexHandler(t *testing.T) {
 	env := Env{DB: &mockDB{}}
 	router := env.NewRouter()
 	router.ServeHTTP(w, r)
+	response := w.Result()
 
-	if status := w.Code; status != http.StatusOK {
+	if status := response.StatusCode; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
+
+	dec := json.NewDecoder(response.Body)
+	var activities []models.Activity
+	err := dec.Decode(&activities)
+	if err != nil {
+		log.Fatal(err)
+	}
+	equals(t, activities[0].Type, "fishing")
+	equals(t, activities[1].Type, "boating")
 }
 
 type mockDB struct{}
@@ -46,5 +62,25 @@ func (mdb *mockDB) AllFacilities() ([]*models.Facility, error) {
 }
 
 func (mdb *mockDB) AllActivities() ([]*models.Activity, error) {
-	return nil, nil
+	activities := make([]*models.Activity, 0)
+	activities = append(activities, &models.Activity{1, "fishing"})
+	activities = append(activities, &models.Activity{2, "boating"})
+	return activities, nil
+}
+
+func ok(tb testing.TB, err error) {
+	if err != nil {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
+		tb.FailNow()
+	}
+}
+
+// equals fails the test if exp is not equal to act.
+func equals(tb testing.TB, exp, act interface{}) {
+	if !reflect.DeepEqual(exp, act) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
+		tb.FailNow()
+	}
 }
